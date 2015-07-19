8086447f01cafb993cd3338cb2a43c4254827d0c
John Halbert <johnhalbert@gmail.com>
2015-07-18 21:59:21 -0700

    Updated angular routes to use ashishes routes

--- client/js/routes.js
+++ client/js/routes.js
@@ -7,51 +7,20 @@ teachy.config(function($routeProvider){
 			templateUrl: 'partials/signup.html'
 		})
 		.when('/signup2',{
-
               templateUrl:'partials/signup2.html'
-
 		})	
 		.when('/signup3',{
-
               templateUrl:'partials/signup3.html'
-
 		})
-
 		.when('/signup4',{
-
               templateUrl:'partials/signup4.html'
-
 		})
 		.when('/home',{
-
               templateUrl:'partials/home.html'
-
 		})
 		.when('/homepage',{
-
               templateUrl:'partials/homepage.html'
-
 		})
-
-		.when('/checkemail', {
-			templateUrl: 'partials/checkemail.html'
-		})
-		.when('/finishsignup1', {
-			templateUrl: 'partials/finishsignup1.html'
-		})
-		.when('/finishsignup2', {
-			templateUrl: 'partials/finishsignup2.html'
-		})
-		.when('/finishsignup3', {
-			templateUrl: 'partials/finishsignup3.html'
-		})
-		.when('/invitecolleagues', {
-			templateUrl: 'partials/invitecolleagues.html'
-		})
-		.when('/chat', {
-			templateUrl: 'partials/chat.html'
-		})
-
 		.otherwise({
 			redirectTo: '/'
 
